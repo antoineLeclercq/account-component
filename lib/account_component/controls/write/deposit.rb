cae656459a6d175afeba8777672b0a90b7174a51
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2023-08-20 13:12:10 -0500

    Write deposit interactive test

--- lib/account_component/controls/write/deposit.rb
+++ lib/account_component/controls/write/deposit.rb
@@ -3,8 +3,8 @@ module AccountComponent
     module Write
       module Deposit
         def self.call(id: nil, account_id: nil, amount: nil)
-          id ||= ID.example
-          account_id ||= Account.id
+          id ||= Identifier::UUID::Random.get
+          account_id ||= Identifier::UUID::Random.get
           amount ||= Money.example
 
           deposit = Commands::Deposit.example(
@@ -16,6 +16,8 @@ module AccountComponent
           stream_name = Messaging::StreamName.command_stream_name(account_id, 'account')
 
           Messaging::Postgres::Write.(deposit, stream_name)
+
+          deposit
         end
       end
     end
--- test/interactive/write_deposit.rb
+++ test/interactive/write_deposit.rb
@@ -1,13 +1,3 @@
 require_relative './interactive_init'
 
-deposit_id = Identifier::UUID::Random.get
-
-account_id = '123'
-
-amount = 11
-
-Controls::Write::Deposit.(
-  id: deposit_id,
-  account_id: account_id,
-  amount: amount
-)
+pp Controls::Write::Deposit.()
